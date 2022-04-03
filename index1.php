825a34dd72cc35c58ab551b95dfaad3cab9e159e
nulesh <talsaniya.krishn@gmail.com>
2022-04-03 13:18:10 +0530

    krishn

---
+++ index1.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Portfolio</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<!-- header setion start -->
+    <header class="header">
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+        <section class="home1" id="home1">
+            <div class="image1">
+                <img src="images/krishn4.jpg" alt="">
+            </div>
+
+    <a href="#home" class="logo">Portfolio.</a>
+
+    <nav class="navbar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#Prtfolio">Portfolio</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+
+    </nav>
+    <div class="follow">
+        
+            <a href="https://www.instagram.com/krishn_3333/" class="fab fa-instagram"></a>
+            <a href="https://wa.me/6357909090?text=hello krishn how are you " class="fab fa-whatsapp"></a>
+            <a href="https://github.com/nulesh/login-system" class="fab fa-github"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+    </div>
+
+</header>
+<!-- header section end -->
+
+<section class="home" id="home">
+  <div class="image">
+      <img src="images/krishn3.jpg" alt="">
+  </div>
+
+    <div class="content">
+        <h2>Hi, I Am Krishn Talsaniya</h2>
+        <span>Wen Designer Frontend And backend Devloper</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Blanditiis rem ipsa eius consectetur consequatur animi, 
+            alias possimus fuga, odit dolores id quibusdam et, ad 
+            perferendis quia. Quas velit eaque nisi.</p>
+
+            <a href="#about" class="btn">About Me</a>
+    </div>
+</section>
+
+<!-- about section start -->
+
+
+<section class="about" id="about">
+
+    <h1 class="heading"><span>Biography</span></h1>
+
+    <div class="biography">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, numquam ut. 
+            Laboriosam aut hic tenetur perferendis blanditiis consequatur et, veniam architecto
+             provident nisi eum molestias, eaque sed! Sint, maiores nobis!</p>
+
+             <div class="bio">
+                 <h3> <span>Name : </span> Krishn Talsaniya </h3>
+                 <h3> <span>Email : </span> talsaniya.krishn@gmail.com </h3>
+                 <h3> <span>Adress : </span> Bhavnager Airport Road </h3>
+                 <h3> <span>Phone : </span>+91 6357909090 </h3>
+                 <h3> <span>Age : </span> 20 years </h3>
+                 <h3> <span>Experience : </span> 1+ year of expierence </h3>
+             </div>
+
+             <a href="download.php?file=krishn" class="btn">Download CV</a>
+    </div>
+</section>
+
+
+<!-- about section end -->
+
+
+
+<script src="js/script.js"></script>
+</body>
+</html>
